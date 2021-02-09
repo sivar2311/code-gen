b780d6353b9eab23d0adab4ce0a83d7bf41bd1f8
sivar2311 <sivar2311@googlemail.com>
2021-02-09 15:32:35 +0100

    fixed MediaController

--- controller/MediaController/callbacks.h
+++ controller/MediaController/callbacks.h
@@ -1,4 +1,4 @@
-bool onLockState(const String &deviceId, String &control) {
+bool onMediaControl(const String &deviceId, String &control) {
   Serial.printf("Device %s: %s\r\n", deviceId.c_str(), control.c_str());
   return true;
 }
