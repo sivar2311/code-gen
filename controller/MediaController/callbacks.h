7756942d7b08b1c49744e35434690c3009c955b1
sivar2311 <sivar2311@googlemail.com>
2021-02-10 10:10:08 +0100

    Variable for MediaController

--- controller/MediaController/callbacks.h
+++ controller/MediaController/callbacks.h
@@ -2,5 +2,6 @@
 
 bool onMediaControl(const String &deviceId, String &control) {
   Serial.printf("Device %s: %s\r\n", deviceId.c_str(), control.c_str());
+  mediaControlState = control;
   return true;
 }
