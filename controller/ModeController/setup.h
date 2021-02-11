7017b50ca4476301ebc534c6afb4b5629f11401d
sivar2311 <sivar2311@googlemail.com>
2021-02-11 15:25:32 +0100

    ModeController

--- controller/ModeController/setup.h
+++ controller/ModeController/setup.h
@@ -1,3 +1,5 @@
   // ModeController
-  <% modeControllerInstances.forEach(element => {%><%=device.deviceInstance%>.onSetMode("<%=element%>", onSetMode);
+
+<% modeControllerInstances.forEach(element => {%>
+  <%=device.deviceInstance%>.onSetMode("<%=element%>", onSetMode);
 <%});%>
